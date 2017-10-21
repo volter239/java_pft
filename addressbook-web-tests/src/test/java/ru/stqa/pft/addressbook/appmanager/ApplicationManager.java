62b1d4b9151dca9aa1bcb5981f379c51af98f0f2
Eugene Oskotsky <volter239@mail.ru>
2017-10-21 20:33:22 +0300

    Урок 3 - Занятие 3. Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,23 +1,50 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        // java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.gecko.driver system property ...
-        System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\ToolsForBrowsers\\geckodriver.exe");
+        if (browser == BrowserType.FIREFOX)
+        {
+            // java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.gecko.driver system property ...
+            System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\ToolsForBrowsers\\geckodriver.exe");
 
-        wd = new FirefoxDriver();
+            wd = new FirefoxDriver();
+        }
+        else if (browser == BrowserType.CHROME)
+        {
+            // java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.chrome.driver system property ...
+            System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\ToolsForBrowsers\\chromedriver.exe");
+
+            wd = new ChromeDriver();
+        }
+        else if (browser == BrowserType.IE)
+        {
+            // java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.ie.driver system property ...
+            System.setProperty("webdriver.ie.driver", "C:\\Program Files\\ToolsForBrowsers\\IEDriverServer.exe");
+
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
         wd.get("http://localhost:8080/addressbook/group.php");
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,10 +3,11 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {
