044064133cec2c2e59766654f47e482372343cba
Eugene Oskotsky <volter239@mail.ru>
2017-10-26 20:09:04 +0300

    Урок 3 - Занятие 9. 'Ленивые' действия

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test21", "test22", "test23"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test21", "test24", "test31"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
