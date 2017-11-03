9fd14c039f3c6c4816f1d4d1aca67e5f5252dbdc
Eugene Oskotsky <volter239@mail.ru>
2017-11-03 15:42:54 +0300

    Урок 4 - Занятие 9. Лямбда-выражения (анонимные функции) и элементы функционального программирования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,21 +14,13 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() {
         app.getNavigationHelper().gotoHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData("F1", "M1", "L1", "N1", "C1", "A1", "PH1", "E1", "test1");
+        ContactData contact = new ContactData("F1.1", "M1", "L1", "N1", "C1", "A1", "PH1", "E1", "test1");
         app.getContactHelper().createContact(contact);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(),before.size()+1);
 
-        int max = 0;
-        for (ContactData g: after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-
-        contact.setId(max);
+        contact.setId(after.stream().max((Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
         before.add(contact);
-
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
