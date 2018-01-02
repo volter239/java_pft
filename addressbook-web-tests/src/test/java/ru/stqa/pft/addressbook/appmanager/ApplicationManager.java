59ef17da0b2e10755b3f14242cc3ff0c445f8542
Eugene Oskotsky <volter239@mail.ru>
2018-01-02 19:04:14 +0300

    Урок 6 - Занятие 10. Описание конфигурации тестового стенда. Сделано задание 16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -19,11 +23,17 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
 
+    private final Properties properties;
+
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (Objects.equals(browser, BrowserType.FIREFOX))
         {
             // java.lang.IllegalStateException: The path to the driver executable must be set by the webdriver.gecko.driver system property ...
@@ -49,7 +59,8 @@ public class ApplicationManager {
         // wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 
-        wd.get("http://localhost:8080/addressbook/");
+        // wd.get("http://localhost:8080/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
 
         groupHelper = new GroupHelper(wd);
 
@@ -58,7 +69,9 @@ public class ApplicationManager {
         navigationHelper = new NavigationHelper(wd);
 
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+
+        //sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    // protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
